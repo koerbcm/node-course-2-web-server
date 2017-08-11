c30f00ddb42e194bf804c75f93fe03fbfd72353b
Chris Koerber <chris.koerber@developm3nt.com>
2017-08-11 13:51:09 -0600

    Add projects page

--- server.js
+++ server.js
@@ -49,6 +49,13 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Here are all of my projects!'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
